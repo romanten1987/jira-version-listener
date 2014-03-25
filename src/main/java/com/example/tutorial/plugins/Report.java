241298e7314c23f7093f8c0e88650a37ce732623
Roman Ten <Roman_Ten@epam.com>
2014-03-25 11:06:29 +0400

    Переделал формирование отчета. Отказался от ReportNote

--- src/main/java/com/example/tutorial/plugins/Report.java
+++ src/main/java/com/example/tutorial/plugins/Report.java
@@ -1,16 +1,19 @@
 package com.example.tutorial.plugins;
 
 import com.atlassian.crowd.embedded.api.User;
+import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.event.project.AbstractVersionEvent;
 import com.atlassian.jira.event.project.VersionCreateEvent;
 import com.atlassian.jira.event.project.VersionReleaseEvent;
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.issuetype.IssueType;
 import com.atlassian.jira.project.Project;
-import com.atlassian.jira.project.util.ReleaseNoteManager;
 import com.atlassian.jira.project.version.Version;
+import com.atlassian.jira.project.version.VersionManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import webwork.action.Action;
-import webwork.action.factory.ActionFactory;
+
+import java.util.Collection;
 
 /**
  * Created by Roman_Ten on 21.03.14.
@@ -18,22 +21,21 @@ import webwork.action.factory.ActionFactory;
 public class Report {
     private static final Logger log = LoggerFactory.getLogger(VersionListener.class);
     public static final String VERSION_RELEASED = "VERSION RELEASED ";
-    public static final String RELEASE_NOTE = "ReleaseNote";
     public static final String SUBJECT_TEMPLATE = "%s %s in %s";
-    public static final String EMAIL_STYLE = "Email";
-    public static final String DEFAULT_STYLE = "Html";
     private static final String VERSION_CREATED = "VERSION CREATED";
     public static final String PROJECT_NAME = "VTBR-TBR2";
     private final VersionListener versionListener;
     private final AbstractVersionEvent versionEvent;
     private final Version version;
     private final Project project;
+    private final VersionManager versionManager;
 
     public Report(VersionListener versionListener, AbstractVersionEvent versionEvent) {
         this.versionListener = versionListener;
         this.versionEvent = versionEvent;
         this.version = getVersion();
         this.project = version.getProjectObject();
+        this.versionManager = versionListener.getVersionManager();
     }
 
 
@@ -55,20 +57,25 @@ public class Report {
     }
 
     public String generateReport() {
-        ReleaseNoteManager releaseNoteManager = new ReleaseNoteManager(versionListener.getApplicationProperties(), versionListener.getEngine(), versionListener.getConstantsManager(), versionListener.getSearchProvider(), versionListener.getCustomFieldManager());
-        String style = EMAIL_STYLE;
-        if (!releaseNoteManager.getStyles().containsKey(EMAIL_STYLE)) {
-            style = DEFAULT_STYLE;
-        }
-        Action action = null;
-        try {
-            ActionFactory actionFactory = ActionFactory.getActionFactory();
-            action = actionFactory.getActionImpl(RELEASE_NOTE);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-        }
+        StringBuilder report = new StringBuilder();
+        String jiraURL = ComponentAccessor.getApplicationProperties().getString("jira.baseurl");
+        Collection<Issue> issues = versionManager.getIssuesWithFixVersion(version);
+        report.append(String.format("<h1><a href=\"%s/browse/%s/fixforversion/%d\">%s</a></h1><p>%s</p>", jiraURL, project.getKey(), version.getId(), version.getName(), version.getDescription()));
+        for (IssueType issueType : project.getIssueTypes()) {
+            StringBuilder text = new StringBuilder();
+            for (Issue issue : issues) {
+                if (issue.getIssueTypeId().equals(issueType.getId())) {
+                    text.append("<li><a href=\"").append(jiraURL).append("/browse/").append(issue.getKey()).append("\">").append(issue.getKey()).append("</a> - ").append(issue.getSummary()).append("</li>");
+                }
 
-        return releaseNoteManager.getReleaseNote(action, style, version, getLeadUser(), project.getGenericValue());
+            }
+            if (!text.toString().equals("")) {
+                report.append("<h2>").append(issueType.getName()).append("</h2><ul>").append(text.toString()).append("</ul>");
+            }
+        }
+        report.insert(0, String.format("<html><title>Release Notes - %s - %s</title><body>", project.getName(), version.getName())).append("</body></html>");
+
+        return report.toString();
     }
 
     public User getLeadUser() {
